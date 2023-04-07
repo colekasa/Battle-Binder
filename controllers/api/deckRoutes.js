c413ba937411ad18b0551a97fdd4a6acf9ab1729
Cole <ce.kasabian@gmail.com>
2023-04-06 21:45:39 -0700

    added routes to deckRoutes

--- controllers/api/deckRoutes.js
+++ controllers/api/deckRoutes.js
@@ -1,34 +1,73 @@
-const {FavoriteList} = require('../../models');
+const { Deck, Card, Cards } = require('../../models');
 const withAuth = require('../../utils/auth');
 const router = require('express').Router();
 
 //CRUD
 
-//create a favorite list 
-router.post('/', withAuth, async(req,res) => {
+//create a Deck
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const listData = await Deck.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+//get all the Cards from the Deck for the logged in user
+router.get('/', withAuth, async (req, res) => {
+  try {
+    const deck = await Deck.findall({
+      include: [
+        {
+          model: Card,
+          through: Cards,
+          as: 'cards',
+        },
+      ],
+    });
+
+    res.status(200).json(deck);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+//update the deck when adding a card
+router.put();
+
+//get one deck
+router.get('/:id', async(req,res) => {
     try{
-        const listData = await FavoriteList.create()
-        
-    }catch(err){
-        res.status(400).json(err)
-    }
-})
-
-
-//get all the favorites from the favoriteslist for the logged in user
-router.get('/', async (req,res) => {
-    try{
-        const favoritesList = await FavoriteList.findall()
-
-        res.status(200).json(favoritesList)
+        const deckData = await Deck.findByPk(req.params.id, {
+            include
+        })
     } catch(err){
         res.status(400).json(err)
     }
-})
+});
 
-//
+//delete the deck
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const deckData = await Deck.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.card_id,
+      },
+    });
 
+    if (!deckData) {
+      res.status(404).json({ message: 'No deck found with this id!' });
+      return;
+    }
 
-
+    res.status(200).json(deckData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
