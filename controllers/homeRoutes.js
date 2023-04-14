db475c6f9f2461ca5a1ed2b5c3d5cda01e64525f
MacKetchup <stylesthestyer@gmail.com>
2023-04-13 21:16:58 -0700

    heroku bug

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,43 +2,6 @@ const router = require('express').Router();
 const { User, Deck, Card } = require('../models');
 const withAuth = require('../utils/auth');
 
-// Use withAuth middleware to prevent access to favorites route unless logged in
-router.get('/deck', withAuth, async (req, res) => {
-  try {
-    // Find the logged in user based on the session ID
-    const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
-      include: [
-        {
-          model: Card,
-          through: Deck,
-          as: 'cards',
-        },
-      ],
-    });
-    // Serialize the data for handlebars to use
-    const user = userData.get({ plain: true });
-    console.log(user);
-    // response renders the deck.handlebars file
-    res.render('deck', {
-      ...user,
-      logged_in: true,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-router.get('/login', (req, res) => {
-  // If the user is already logged in, redirect the request to the deck page
-  if (req.session.logged_in) {
-    res.redirect('/deck');
-    return;
-  }
-
-  res.render('login');
-});
-
 router.get('/', async (req, res) => {
   const cards = await Card.findAll({ attributes: ['id'] });
 
@@ -76,6 +39,43 @@ router.get('/', async (req, res) => {
   res.render('homepage', {
     randomCard: testCard,
   });
+});
+
+// Use withAuth middleware to prevent access to favorites route unless logged in
+router.get('/deck', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [
+        {
+          model: Card,
+          through: Deck,
+          as: 'cards',
+        },
+      ],
+    });
+    // Serialize the data for handlebars to use
+    const user = userData.get({ plain: true });
+    console.log(user);
+    // response renders the deck.handlebars file
+    res.render('deck', {
+      ...user,
+      logged_in: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to the deck page
+  if (req.session.logged_in) {
+    res.redirect('/deck');
+    return;
+  }
+
+  res.render('login');
 });
 
 module.exports = router;
